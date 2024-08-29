c67817bfcafd9fee91e67c3fdc6883e3527f2623
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2024-08-29 20:52:28 +0300

    installed mongoDb and refactored that registration will save user to db

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -1,18 +1,29 @@
 const express = require('express');
-const router = express.Router();
 
-// Handle POST request to /submit-registration
-router.post('/submit-registration', (req, res) => {
+module.exports = (User) => {
+  const router = express.Router();
+
+  // Handle POST request to /submit-registration
+  router.post('/submit-registration', (req, res) => {
     const { email, password, repeatPassword } = req.body;
 
     if (password !== repeatPassword) {
-        return res.status(400).send('Passwords do not match!');
+      return res.status(400).send('Passwords do not match!');
     }
 
-    // Process form data (e.g., save to database)
-    console.log('Email:', email);
-    console.log('Password:', password);
-    res.send('Form submitted successfully!');
-});
+    // Save form data to MongoDB
+    const newUser = new User({
+      email: email,
+      password: password
+    });
 
-module.exports = router;
+    newUser.save((err) => {
+      if (err) {
+        return res.status(500).send('Error saving data!');
+      }
+      res.send('Form submitted successfully!');
+    });
+  });
+
+  return router;
+};
